ac7e8861768ef6acc2059602ddf0f04e5a96a605
BautistaBoselli <bboselli@fi.uba.ar>
2024-07-13 16:49:03 -0300

    Punto de partida

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,10 @@
 export default function Home() {
   return (
-    <main className="h-screen bg-slate-100 p-10 flex flex-col gap-4">
+    <main className="h-screen bg-slate-200 p-10 flex flex-col gap-4">
       <h1 className="text-xl text-slate-700 font-bold">Cuadratic</h1>
-      <div className="min-h-40 max-w-2xl  rounded-lg bg-white">Aca van las tareas...</div>
+      <div className="min-h-40 max-w-2xl  rounded-lg bg-white">
+        Punto de partida
+      </div>
     </main>
   );
 }
